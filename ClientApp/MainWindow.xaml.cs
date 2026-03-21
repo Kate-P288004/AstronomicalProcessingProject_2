e9fddcad9a993337f4acd6a91d840b1be7c0386f
Kate <kate.odabas@gmail.com>
2026-03-21 17:43:42 +0800

    Implement XAML UI layout and button click handlers

--- ClientApp/MainWindow.xaml.cs
+++ ClientApp/MainWindow.xaml.cs
@@ -8,17 +8,81 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AstroProto;
+
 
 namespace ClientApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Language
+        private void English_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void French_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void German_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // Theme
+        private void LightTheme_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DarkTheme_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BlueTheme_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // Customise
+        private void BackgroundColour_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LabelColour_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void TextBoxColour_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Font_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // About
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // Calculation buttons
+        private void BtnVelocity_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnDistance_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnTemp_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnRadius_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
